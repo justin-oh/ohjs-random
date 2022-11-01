608e211306ec8b878d839e6c4ba78c060410b350
Justin Hoffman <justin@ohmedia.ca>
2022-11-01 10:59:36 -0600

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+A Javascript module that provides 2 functions: 1 for generating a random 
+integer and 1 for generating a random string.
+
+Basic usage:
+
+```js
+import {
+    randomInteger,
+    randomString,
+} from '/path/to/random-js/index.js';
+
+const randomInt = randomInteger(0, 10);
+
+const randomStr = randomString(20);
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+import { isFunction } from '../is-js/index.js';
+
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = lowercase.toUppercase();
+const numbers = '0123456789';
+const characters = lowercase + uppercase + numbers;
+
+export function randomInteger(min, max) {
+    if (!Number.isInteger(min)) {
+        throw '`min` should be an integer';
+    }
+
+    if (!Number.isInteger(max)) {
+        throw '`max` should be an integer';
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function randomString(length, verify) {
+    if (!Number.isInteger(length)) {
+        throw '`length` should be an integer';
+    }
+
+    // make sure it starts with a lowercase letter
+    let str = lowercase[randomInteger(0, lowercase.length - 1)];
+
+    for (let i = 1; i < length; i++) {
+        str += characters[randomInteger(0, characters.length - 1)];
+    }
+
+    return str;
+}
